1a47d972677d1d6c4b6e4812c53b3de513f07dc6
Iain Barnett <iainspeed@gmail.com>
2012-07-04 20:09:05 +0100

    Added some of the normal set up for an extension.

--- lib/sinatra/MODULENAME.rb
+++ lib/sinatra/MODULENAME.rb
@@ -1,7 +1,13 @@
 # encoding: UTF-8
 
+require 'sinatra/base'
+require 'sinatra/extension'
+
 module Sinatra
   module MODULENAME
+    extend Sinatra::Extension
+    
     # Your code goes here...
+
   end
 end
