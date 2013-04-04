ec555c7b06f177dc14beb6d39926e9cfd6c4d329
Iain Barnett <iainspeed@gmail.com>
2013-04-04 13:33:24 +0100

    Removed dangling file that added a top level constant, and some code comments that were unnecessary.

--- spec/support/shared/all_routes.rb
+++ spec/support/shared/all_routes.rb
@@ -2,14 +2,12 @@
 
 require 'sinatra/base'
 require_relative "../../../lib/sinatra/default_parameters.rb"
-# require_relative "./helpers.rb"
 
 require_relative "../../../examples/app.rb"
 
 shared_context "All pages" do
   include Rack::Test::Methods
   let(:app){ Example }
-  #include MODULE::RSpec::Helpers
 end
 
 shared_examples_for "Any route" do
